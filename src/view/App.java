cbe99125adc54e14eb019cd52adbe240b5d09ecd
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-09-21 14:34:09 -0300

    CRUD completo

--- src/view/App.java
+++ src/view/App.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import data.UsuarioData;
@@ -33,8 +34,30 @@ public class App {
                         else System.out.println("\n\n######### Não salvou ########\n\n");
                         break;
                     case 2:
+                        listarDados(DAO);
+                        break;
+                    case 3:
+                        listarDados(DAO);
+                        System.out.println("Digite o id do usuário a ser excluído:");
+                        obj.setId(entrada.nextInt());
+                        System.out.println("Digite o nome:");
+                        obj.setNome(entrada.next());
+                        System.out.println("Digite o e-mail:");
+                        obj.setEmail(entrada.next());
+                        System.out.println("Digite o tipo [1-ADM | 2-Cliente]:");
+                        obj.setTipo(entrada.nextInt());
+                        if(DAO.editar(obj)) System.out.println("\n\nAtualizado!\n\n");
+                        else System.out.println("\n\n######### Não atualizou ########\n\n");
                         
                         break;
+                    case 4:
+                        listarDados(DAO);
+                        System.out.println("Digite o id do usuário a ser excluído:");
+                        int id = entrada.nextInt();
+                        if(DAO.excluir(id)) System.out.println("#### EXCLUÍDO ####");
+                        else System.out.println("### Não foi excluído ###");
+                        break;
+                        
                     default:
                         break;
                 }
@@ -42,5 +65,14 @@ public class App {
                 System.out.println("Erro:"+e.getMessage());
             }
         } while (opcao!=9);
+    }
+
+    private static void listarDados(UsuarioData DAO) throws Exception{
+        ArrayList<UsuarioModel> dados = DAO.pesquisar("%");
+        System.out.println("\n\n############# LISTAGEM ##############");
+        for (UsuarioModel u : dados) {
+            System.out.println("\nId:"+ u.getId() + ": "+u.getNome()+
+            "\nE-mail:"+u.getEmail() + " --> " + u.getTipo());
+        }
     }
 }
